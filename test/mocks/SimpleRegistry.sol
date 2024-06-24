82433755084798268dd0b8e1de5208bc26df6f2c
Kresh <andrey@symbiotic.fi>
2024-06-24 23:09:17 +0400

    minors

---
+++ test/mocks/SimpleRegistry.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.25;
+
+import {Registry} from "src/contracts/common/Registry.sol";
+
+contract SimpleRegistry is Registry {
+    function register() external returns (address) {
+        _addEntity(msg.sender);
+        return msg.sender;
+    }
+}
