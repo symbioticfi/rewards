d1eece789d7fd5ec4d5e29c0c043edc9ab17809a
Kresh <andrey@symbiotic.fi>
2024-10-30 10:08:19 +0400

    feat: visualize deployed addresses & add general script

--- script/deploy/DefaultRewardsFactories.s.sol
+++ script/deploy/DefaultRewardsFactories.s.sol
@@ -6,7 +6,11 @@ import {console2, Script} from "forge-std/Script.sol";
 import {DefaultStakerRewards} from "../../src/contracts/defaultStakerRewards/DefaultStakerRewards.sol";
 import {DefaultStakerRewardsFactory} from "../../src/contracts/defaultStakerRewards/DefaultStakerRewardsFactory.sol";
 
-contract DefaultStakerRewardsFactoryScript is Script {
+import {DefaultOperatorRewards} from "../../src/contracts/defaultOperatorRewards/DefaultOperatorRewards.sol";
+import {DefaultOperatorRewardsFactory} from
+    "../../src/contracts/defaultOperatorRewards/DefaultOperatorRewardsFactory.sol";
+
+contract DefaultRewardsFactoriesScript is Script {
     function run(address vaultFactory, address networkMiddlewareService) external {
         vm.startBroadcast();
 
@@ -15,7 +19,12 @@ contract DefaultStakerRewardsFactoryScript is Script {
         address defaultStakerRewardsFactory =
             address(new DefaultStakerRewardsFactory(address(stakerRewardsImplementation)));
 
+        DefaultOperatorRewards operatorRewardsImplementation = new DefaultOperatorRewards(networkMiddlewareService);
+        address defaultOperatorRewardsFactory =
+            address(new DefaultOperatorRewardsFactory(address(operatorRewardsImplementation)));
+
         console2.log("Default Staker Rewards Factory: ", defaultStakerRewardsFactory);
+        console2.log("Default Operator Rewards Factory: ", defaultOperatorRewardsFactory);
 
         vm.stopBroadcast();
     }
